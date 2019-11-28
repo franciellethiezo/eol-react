9199368f9cb2c3f569a431ea5260288ce407ad1c
rafaelgckto <rafael.gcecatto@gmail.com>
2019-11-28 17:04:48 -0300

    ajustar final do html e css do Template Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -27,33 +27,44 @@ class Login extends Component {
             <div>
                 <Cabecalho />
 
-                <main className="flex-center">
-                    <section className="box-cad">
+                <main className="main-login flex-center">
+                    <section className="box-cad flex-center">
                         <div className="d-box flex-column">
-                            <h2>Login</h2>
-                            <p>
-                                Faça o login no <span><img src={Logo} alt="logo" /></span>
+                            <h2 className="h2-login">Login</h2>
+                            <p className="p-title-login">
+                                Faça o login no <span><img className="img-logo-login" src={Logo} alt="logo" /></span>
                             </p>
                             <form action="#" className="form-box flex-column">
-                                <input type="text" name="e-mail" placeholder="E-mail corporativo" />
-                                <input type="text" name="senha" placeholder="Senha" />
+                                <input className="input-form-login" type="text" name="e-mail" placeholder="E-mail corporativo" />
+                                <input className="input-form-login" type="text" name="senha" placeholder="Senha" />
                                 <div className="remind-box flex-column">
-                                    <div className="remind flex-between">
-                                        <input type="checkbox" name="remind_pass" id="remind_input" /><label for="remind_pass">Lembrar de mim</label>
+                                    <div className="remind flex-center">
+                                        <input id="remind-input" type="checkbox" name="remind-pass" />
+                                        <label className="cbx" for="remind-input">
+                                            <div className="flip">
+                                                <div className="front"></div>
+                                                <div className="back">
+                                                    <svg className="svg-cbx" width="16" height="14" viewBox="0 0 16 14">
+                                                        <path className="path-cbx" d="M2 8.5L6 12.5L14 1.5"></path>
+                                                    </svg>
+                                                </div>
+                                            </div>
+                                        </label>
+                                        <label className="lbl-remember-me">Lembrar de mim</label>
                                     </div>
-                                    <a href="#">Esqueci minha senha</a>
+                                    <a className="forgot-it-password" href="#">Esqueci minha senha</a>
                                 </div>
-                                <button className="roxo btn-cad" onclick=" window.location.href = '\adverts.html';">entrar</button>
+                                <button className="roxo btn-cad">Entrar</button>
                             </form>
-                            <div className="rambo">
+                            <div className="box-footer">
                                 <div className="hr-log flex-center">
                                     <hr />
-                                    <p>ou</p>
+                                    <p className="p-box-log">ou</p>
                                     <hr />
                                 </div>
                                 <div className="flex-column">
                                     <p>Não tem uma conta?</p>
-                                    <a href="./cadastro-caixa.html" className="lnk-entrar">faça seu cadastro</a>
+                                    <a href="#" className="lnk-entrar">faça seu cadastro</a>
                                 </div>
                             </div>
                         </div>
