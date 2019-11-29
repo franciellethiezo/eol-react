9ea01ead4b62701fee4f5eee8baf22717a1967c3
rafaelgckto <rafael.gcecatto@gmail.com>
2019-11-29 09:42:57 -0300

    ajuste do html e css CadastroUsuario, versão desktop

--- src/pages/CadastroUsuario.js
+++ src/pages/CadastroUsuario.js
@@ -5,7 +5,7 @@ import Cabecalho from '../components/Cabecalho';
 import Rodape from '../components/Rodape';
 
 // Css (assets/css)
-import '../assets/css/login.css';
+import '../assets/css/cadastro-usuario.css';
 import '../assets/css/style.css';
 
 // Imagens (assets/images)
@@ -29,19 +29,19 @@ class CadastroUsuario extends Component {
 
                 <main className="flex-center">
                     <section className="box-cad">
-                        <div className="d-box flex-column">
-                            <h2>Cadastre-se</h2>
-                            <p>Faça o cadastro no <span><img className="img-logo-cad" src={Logo} alt="logo" /></span></p>
-                            <form action="#" className="form-box flex-column">
-                                <input type="text" name="nome" placeholder="Nome completo" />
-                                <input type="text" name="e-mail" placeholder="E-mail corporativo" />
-                                <input type="text" name="senha" placeholder="Senha" />
-                                <input type="text" name="confirmação" placeholder="Confirmação de senha" />
-                                <input type="text" name="telefone" placeholder="Telefone" />
+                        <div className="d-box-cad flex-column">
+                            <h2 className="h2-cad">Cadastre-se</h2>
+                            <p className="p-cad">Faça o cadastro no <span><img className="img-logo-cad" src={Logo} alt="logo" /></span></p>
+                            <form action="#" className="form-box-cad flex-column">
+                                <input className="input-cad" type="text" name="nome" placeholder="Nome completo" />
+                                <input className="input-cad" type="text" name="e-mail" placeholder="E-mail corporativo" />
+                                <input className="input-cad" type="text" name="senha" placeholder="Senha" />
+                                <input className="input-cad" type="text" name="confirmação" placeholder="Confirmação de senha" />
+                                <input className="input-cad" type="text" name="telefone" placeholder="Telefone" />
                                 <button className="roxo btn-cad">Cadastre-se</button>
                             </form>
-                            <p>já possui uma conta?</p>
-                            <a href="./login-caixa.html" className="a-cad lnk-entrar">entrar</a>
+                            <p className="p-cad-footer">Já possui uma conta?</p>
+                            <a href="" className="a-cad lnk-entrar">Entrar</a>
                         </div>
                     </section>
                 </main>
