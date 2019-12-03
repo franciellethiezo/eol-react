d226548a2f28ee6b09d26933e2b72bce56e89bb7
rafaelgckto <rafael.gcecatto@gmail.com>
2019-12-03 16:16:21 -0300

    finalizar versão 2.0 desktop do componente NavBarUser

--- src/components/NavbarUser.js
+++ src/components/NavbarUser.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // Css (assets/css)
 import '../assets/css/navbar-user.css';
@@ -16,62 +16,79 @@ import HandShake from '../assets/svg/NavBarUser/handshake-icon.svg';
 import LogOut from '../assets/svg/NavBarUser/logout-icon.svg';
 
 //Script JS
-import { myFunction, myFunction2 } from '../assets/js/navbar';
+// import { toggleMegaphone } from '../assets/js/navbar';
 
-function NavBarUser() {
-    return (
-        <div>
-            <div className="dark-modal flex-center" id="dark-modal"></div>
-            <aside className="aside-main flex-column-between" id="aside-mobile">
-                <div className="aside-from-above">
-                    <div className="aside-logo flex-center">
-                        <img className="aside-img-logo" src={Logo} alt="" />
-                        {/* <i class="fas fa-times" onclick="toggleAside()"></i> */}
-                    </div>
-                    <div className="aside-icons">
-                        <div className="aside-sup-navigation flex-center" onclick="toggleAside()">
-                            <a className="aside-a-icons megaphone-a flex-center" href="#">
-                                <img id="megaphone" className="aside-icons-box megaphone" src={Megaphone} alt="Aba de Anúncios" />
-                                <div className="aside-text-icons aside-box-icons megaphone-text">Anúncios</div>
-                            </a>
-                        </div>
-                        <div className="aside-sup-navigation flex-center">
-                            <a className="aside-a-icons heart-a flex-center" href="#">
-                                <img className="aside-icons-box" src={Heart} alt="Aba de Seus Interesses" />
-                                <div className="aside-text-icons aside-box-icons heart-text">Lista de Interesses</div>
-                            </a>
-                        </div>
-                        <div className="aside-sup-navigation flex-center">
-                            <a className="aside-a-icons hand-shake-a flex-center" href="#">
-                                <img className="aside-icons-box" src={HandShake} alt="Aba de Compras realizadas" />
-                                <div className="aside-text-icons aside-box-icons hand-shake-text">Compras Efetuadas</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div className="aside-under">
-                    <div className="aside-logout aside-bottom-navigation flex-center">
-                        <a className="aside-a-btn flex-center" href="#">
-                            <img className="aside-img-logout" src={LogOut} alt="" />
-                            <div className="aside-text-logout aside-box-logout">Logout</div>
-                        </a>
-                    </div>
-                    <div className="aside-profile flex-center">
-                        <a href="#">
-                            <img className="aside-img-profile" src={CSharp} alt="" />
-                        </a>
-                    </div>
-                </div>
+class NavBarUser extends Component {
+    constructor() {
+        super();
+        this.state = {
+            megaphone: "aside-icons-box"
+        }
+    }
 
-                {/* <div class="aside-bottom flex-column">
-                    <div class="aside-profile flex-center">
-                        <div class="aside-img-profile background-center"></div>
+    toggleMegaphone() {
+        const element = document.querySelector('#megaphone');
+
+        if (element) {
+            this.setState({ megaphone: "aside-icons-box active" });
+        }
+        // else{
+        //     this.setState({ megaphone: "aside-icons-box" });
+        // }
+    }
+
+    componentDidMount(){
+        
+    }
+
+    render() {
+        return (
+            <div>
+                {/* <div className="dark-modal flex-center" id="dark-modal"></div> */}
+                <aside className="aside-main flex-column-between" id="aside-mobile">
+                    <div className="aside-from-above">
+                        <div className="aside-logo flex-center">
+                            <img className="aside-img-logo" src={Logo} alt="" />
+                        </div>
+                        <div className="aside-icons">
+                            <div className="aside-sup-navigation flex-center">
+                                <a className="aside-a-icons megaphone-a flex-center" href="#">
+                                    <img id="megaphone" className={this.state.megaphone} src={Megaphone} alt="Aba de Anúncios" onClick={this.toggleMegaphone.bind(this)} />
+                                    <div className="aside-text-icons aside-box-icons megaphone-text">Anúncios</div>
+                                </a>
+                            </div>
+                            <div className="aside-sup-navigation flex-center">
+                                <a className="aside-a-icons heart-a flex-center" href="#">
+                                    <img className="aside-icons-box" src={Heart} alt="Aba de Seus Interesses" />
+                                    <div className="aside-text-icons aside-box-icons heart-text">Lista de Interesses</div>
+                                </a>
+                            </div>
+                            <div className="aside-sup-navigation flex-center">
+                                <a className="aside-a-icons hand-shake-a flex-center" href="#">
+                                    <img className="aside-icons-box" src={HandShake} alt="Aba de Compras realizadas" />
+                                    <div className="aside-text-icons aside-box-icons hand-shake-text">Compras Efetuadas</div>
+                                </a>
+                            </div>
+                        </div>
                     </div>
-                    <img class="aside-thought-logo" src={LogoTW} alt="" />
-                </div> */}
-            </aside>
-        </div>
-    );
+                    <div className="aside-under">
+                        <div className="aside-logout aside-bottom-navigation flex-center">
+                            <a className="aside-a-btn logout-link flex-center" href="#">
+                                <img className="aside-img-logout" src={LogOut} alt="Finalizar " />
+                                <div className="aside-text-logout logout-text aside-box-logout">Logout</div>
+                            </a>
+                        </div>
+                        <div className="aside-profile aside-bottom-navigation flex-center">
+                            <a className="aside-a-btn profile-link flex-center" href="#">
+                                <img className="aside-img-profile" src={CSharp} alt="Página de perfil do usuário" />
+                                <div className="aside-text-logout profile-text aside-box-logout">Perfil</div>
+                            </a>
+                        </div>
+                    </div>
+                </aside>
+            </div>
+        );
+    }
 }
 
 export default NavBarUser;
