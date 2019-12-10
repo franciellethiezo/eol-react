059694805aff06ebc37c7d8188a58a6cd0fa31d7
rafaelgckto <rafael.gcecatto@gmail.com>
2019-12-10 16:12:03 -0300

    criar versão 3.0 da resposividade da página Perfil.js-prefil.css

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -4,10 +4,16 @@ import React, { Component } from 'react';
 import NavBarUser from '../components/NavbarUser';
 
 // Css (assets/css)
-// import '../assets/css/perfil.css';
+import '../assets/css/perfil.css';
 import '../assets/css/style.css';
 
-class Perfil extends Component {
+// Images
+import CSharp from '../assets/img/c-sharp.png';
+
+// SVG
+import ImgProfile from '../assets/svg/Profile/user.svg';
+
+export default class Perfil extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -20,95 +26,61 @@ class Perfil extends Component {
 
     render() {
         return (
-            <section className="web-app">
-                <NavBarUser />
-                
-                {/* <section className="app-page" id="app-page-produto">
-                    <nav class="mobile-nav flex-center">
-                        <div class="wrap container flex-between">
-                            <div class="hamburger-menu" onclick="toggleAside()">
-                                <div class="line line-0"></div>
-                                <div class="line line-1"></div>
-                                <div class="line"></div>
-                            </div>
-                            <img class="logo" src="./imgs/logo.png" alt="" />
-                            <img class="profile" src="./imgs/profile-square.png" alt="" />
-                        </div>
-                    </nav>
-
-
-                    <main>
-                        <header class="titulo_perfil flex-center" id="header-perfil">
-                            <h1 class="flex-align-center">
-                                <span><i class="far fa-user"></i></span>Perfil
-                        </h1>
-                            <div class="cont-img flex-space-evenly">
-                                <div class="image-profile background-center">
-                                    <img src="./img/Logo Oficial.png" alt="" />
+            <div>
+                <section id="profile-app-page" className="profile-app-page">
+                    <NavBarUser />
+                    <main className="profile-main">
+                        <div id="profile-header" className="profile-div-header">
+                            <div className="profile-div flex-around">
+                                <h1 className="profile-h1-header">
+                                    <span><img className="profile-img-header" src={ImgProfile} alt="Ícone de Perfil" /></span>Perfil
+                                </h1>
+                                <div className="profile-box-img">
+                                    <img className="profile-img-account" src={CSharp} alt="Foto de Perfil" />
                                 </div>
                             </div>
-                        </header>
-                        <section class="up-info flex-justify-center">
-                            <section class="info-dados flex-space-evenly">
-                                <div class="up-left flex-column">
-                                    <div class="nome flex-direction-column">
-                                        <label for="">Nome</label>
-                                        <input type="text" name="" id="" />
-                                    </div>
-                                    <div class="tel flex-direction-column">
-                                        <label for="">Telefone</label>
-                                        <input type="text" name="" id="" />
-                                    </div>
-                                </div>
-                                <div class="up-right flex-column">
-                                    <div class="email flex-direction-column">
-                                        <label for="">Email</label>
-                                        <input type="text" name="" id="" />
-                                    </div>
-                                    <div class="notif flex-direction-column">
-                                        <div class="bb flex-align-center">
-                                            <label class="switch">
-                                                <input type="checkbox" />
-                                                <span class="slider round"></span>
-                                            </label>
-                                            <p>Notificação A</p>
-                                        </div>
-                                        <div class="bb flex-align-center">
-                                            <label class="switch">
-                                                <input type="checkbox" />
-                                                <span class="slider round"></span>
-                                            </label>
-                                            <p>Notificação B</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </section>
-                        </section>
-                        <div class="save-config flex-justify-align">
-                            <input type="button" value="Salvar Configurações" />
                         </div>
-                        <hr />
-                        <h2 id="change_pass">Alterar Senha</h2>
-                        <section class="password flex-justify-center">
-                            <section class="pass-data flex-space-evenly">
-                                <div class="new-pass flex-direction-column">
-                                    <label for="">Nova Senha</label>
-                                    <input type="text" name="" id="" />
+                        <form className="profile-form-data">
+                            <div className="profile-form">
+                                <div className="profile-form-left flex-column">
+                                    <div className="profile-form-name">
+                                        <label className="profile-labels">Nome</label>
+                                        <input className="profile-inputs" type="text" />
+                                    </div>
+                                    <div className="profile-form-telephone">
+                                        <label className="profile-labels">Telefone</label>
+                                        <input className="profile-inputs" type="text" />
+                                    </div>
                                 </div>
-                                <div class="confirm-pass flex-direction-column">
-                                    <label for="">Confirme a Senha</label>
-                                    <input type="text" name="" id="" />
+                                <div className="profile-form-right">
+                                    <div className="profile-form-email">
+                                        <label className="profile-labels">E-mail</label>
+                                        <input className="profile-inputs" type="text" />
+                                    </div>
+                                    <button className="profile-btns">Salvar Configurações</button>
                                 </div>
-                            </section>
-                        </section>
-                        <div class="save-config flex-justify-align">
-                            <input type="button" value="Alterar Senha" />
-                        </div>
+                            </div>
+                        </form>
+                        <hr className="profile-hr" />
+                        <form className="profile-form-two">
+                            <h2 className="profile-change-pass">Alterar Senha</h2>
+                            <div className="profile-box-down">
+                                <div className="profile-down">
+                                    <div className="profile-form-new">
+                                        <label className="profile-labels">Nova Senha</label>
+                                        <input className="profile-inputs" type="text" />
+                                    </div>
+                                    <div className="profile-form-confirm">
+                                        <label className="profile-labels">Confirme a Senha</label>
+                                        <input className="profile-inputs" type="text" />
+                                    </div>
+                                </div>
+                            </div>
+                            <button className="profile-btns">Alterar Senha</button>
+                        </form>
                     </main>
-                </section> */}
-            </section>
+                </section>
+            </div>
         );
     }
 }
-
-export default Perfil;
